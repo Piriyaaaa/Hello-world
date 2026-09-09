2f409142bb0126764a232a0e65ba0797ff45f6a8
Piriyaaaa <fakhteh01@protonmail.com>
2026-09-09 14:36:00 +0400

    Add files via upload

---
+++ Order.java
@@ -0,0 +1,35 @@
+package okx;
+
+import com.neovisionaries.ws.client.*;
+import org.json.JSONObject;
+import org.json.JSONArray;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
+
+public class Order {
+
+	public static void main(String[] args) {
+		
+		double Price = 0;
+		int Quantity = 0;
+		
+		System.out.println(Price);
+		System.out.println(Quantity);
+					
+				
+			}
+	public class orderbook{
+		
+	ArrayList Listbids = new ArrayList();
+	ArrayList Listasks = new ArrayList();
+	
+	}
+	
+	public static class WebSocketDemo{
+		public static void main(String[] args) {
+		
+	}
+	}
+}
+
