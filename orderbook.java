f1356a8b039559927bb4776e9a4e065f6d1125de
Piriyaaaa <fakhteh01@protonmail.com>
2026-09-19 18:47:50 +0400

    Add files via upload

---
+++ orderbook.java
@@ -0,0 +1,23 @@
+package okx;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import org.json.JSONObject;
+
+
+public class orderbook {
+	String price = "message";
+	private BigDecimal bids;
+	private BigDecimal asks;
+	
+	ArrayList <BigDecimal> bid1 = new ArrayList<>();
+	ArrayList <BigDecimal> ask1 = new ArrayList<>();
+	
+	
+	
+	
+	
+	
+	
+
+}
