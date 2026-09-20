78cf46a98c4e5d1b7a80e5817b2c71a4b820a7fc
Piriyaaaa <fakhteh01@protonmail.com>
2026-09-20 11:22:39 +0400

    Refactor orderbook class to initialize lists in main
    Moved the ArrayList declarations to the main method and added print statements.

--- orderbook.java
+++ orderbook.java
@@ -4,27 +4,21 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import org.json.JSONObject;
 
-
 public class orderbook {
-	
+
 	orderbook orderbook = new orderbook();
 	String price = "message";
 	private BigDecimal bids;
 	private BigDecimal asks;
-	
-	//generics
-	ArrayList <BigDecimal> bid1 = new ArrayList<BigDecimal>();
-	ArrayList <BigDecimal> ask1 = new ArrayList<BigDecimal>();
-	
-	bid1.add(price);
-	ask1.add(price);
-	
-	
-	
-	
-	
-	
-	
-	}	
 
+	public static void main(String[] args) {
+		// generics
+		ArrayList<BigDecimal> bid1 = new ArrayList<>();
+		ArrayList<BigDecimal> ask1 = new ArrayList<>();
 
+		System.out.println(bid1.get(0));
+		System.out.println(ask1.get(0));
+
+	}
+
+}
